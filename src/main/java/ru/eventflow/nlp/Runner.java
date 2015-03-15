7447665d474364dfdf065b8854afea129d17e04c
vtqveant <vtqveant@localhost>
2015-03-15 14:24:17 +0300

    fts config and basic query

--- src/main/java/ru/eventflow/nlp/Runner.java
+++ src/main/java/ru/eventflow/nlp/Runner.java
@@ -7,16 +7,17 @@ import ru.eventflow.nlp.xml.Text;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -25,18 +26,21 @@ public class Runner {
     private static Unmarshaller unmarshaller;
 
     private static final Logger logger = Logger.getLogger("Runner");
+    private static EntityManager em;
+
+    static {
+        em = Persistence.createEntityManagerFactory("track1").createEntityManager();
+        configureFTS();
+    }
 
     public static void main(String[] args) throws Exception {
         JAXBContext jc = JAXBContext.newInstance("ru.eventflow.nlp.xml");
         unmarshaller = jc.createUnmarshaller();
 
-//        URL url = new URL("file:///home/transcend/code/NLU-RG/tracks/track1/data/annot.opcorpora.xml.byfile/" + FILENAME);
-
-        EntityManager em = Persistence.createEntityManagerFactory("track1").createEntityManager();
         em.getTransaction().begin();
-
-        File corpusDirectory = new File("/home/transcend/code/NLU-RG/tracks/track1/data/annot.opcorpora.xml.annot.opcorpora.xml.byfile");
-        for (File f : corpusDirectory.listFiles()) {
+        URL resoursesLocation = Runner.class.getResource("/annot.opcorpora.xml.byfile");
+        for (File f : new File(resoursesLocation.getFile()).listFiles()) {
+            if (!f.getName().endsWith(".xml")) continue;
             try {
                 String id = f.getName().substring(0, f.getName().indexOf(".xml"));
                 Document doc = processFile(id, f.getAbsolutePath());
@@ -45,15 +49,24 @@ public class Runner {
                 logger.info(f.getName());
             }
         }
-
         em.getTransaction().commit();
 
-        // пример поискового запроса в PostgreSQL:
-        // SELECT * FROM "document" WHERE "text" @@ 'юбилею' limit 10;
 
-        // настройка текстовой конфигурации
+        String[] queries = new String[]{"книга", "президент предложил", "президенты предложили"};
+        for (String q : queries) {
+            List<Integer> ids = executeQuery(q);
+            System.out.println(q + ": " + ids);
+        }
+    }
 
-
+    private static void configureFTS() {
+        em.getTransaction().begin();
+        em.createNativeQuery("DROP TEXT SEARCH CONFIGURATION IF EXISTS public.fts_config;").executeUpdate();
+        em.createNativeQuery("DROP TEXT SEARCH DICTIONARY IF EXISTS public.russian_stem;").executeUpdate();
+        em.createNativeQuery("CREATE TEXT SEARCH CONFIGURATION public.fts_config ( COPY=pg_catalog.russian );").executeUpdate();
+        em.createNativeQuery("CREATE TEXT SEARCH DICTIONARY public.russian_stem ( TEMPLATE=snowball, language='russian', stopwords='russian' );").executeUpdate();
+        em.createNativeQuery("SET default_text_search_config = 'public.fts_config';").executeUpdate();
+        em.getTransaction().commit();
     }
 
     private static Document processFile(String id, String filename) throws IOException, JAXBException {
@@ -84,6 +97,7 @@ public class Runner {
         doc.setId(new Integer(id));
 
         // flush to disk
+        /*
         File fullTextFile = new File("/tmp/opcorpora/docs/" + id + ".txt");
         if (!fullTextFile.getParentFile().exists()) {
             fullTextFile.getParentFile().mkdirs();
@@ -91,8 +105,14 @@ public class Runner {
         FileOutputStream fout = new FileOutputStream(fullTextFile);
         fout.write(sb.toString().getBytes());
         fout.close();
+        */
 
         return doc;
+    }
 
+    @SuppressWarnings("unchecked")
+    private static List<Integer> executeQuery(String query) {
+        Query q = em.createNativeQuery("SELECT id FROM document WHERE text @@ '" + query + "'", Integer.class);
+        return (List<Integer>) q.getResultList();
     }
 }

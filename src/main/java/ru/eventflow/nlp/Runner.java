b324236906a6e4677728a99ab039938399c6e49f
vtqveant <vtqveant@localhost>
2015-03-15 17:57:43 +0300

    refac

--- src/main/java/ru/eventflow/nlp/Runner.java
+++ src/main/java/ru/eventflow/nlp/Runner.java
@@ -13,9 +13,7 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +21,15 @@ import java.util.logging.Logger;
 
 public class Runner {
 
-    private static Unmarshaller unmarshaller;
+    public static final String FULL_DUMP = "annot.opcorpora.xml.byfile";
+    public static final String GOLD_STANDARD = "gold";
 
     private static final Logger logger = Logger.getLogger("Runner");
+
+    private static Unmarshaller unmarshaller;
     private static EntityManager em;
+
+    private static final boolean FLUSH = false;
 
     static {
         em = Persistence.createEntityManagerFactory("track1").createEntityManager();
@@ -37,20 +40,28 @@ public class Runner {
         JAXBContext jc = JAXBContext.newInstance("ru.eventflow.nlp.xml");
         unmarshaller = jc.createUnmarshaller();
 
+        URL resourcesLocation = Runner.class.getResource("/" + GOLD_STANDARD);
+        List<File> files = scanResources(new File(resourcesLocation.getFile()));
+
         em.getTransaction().begin();
-        URL resoursesLocation = Runner.class.getResource("/annot.opcorpora.xml.byfile");
-        for (File f : new File(resoursesLocation.getFile()).listFiles()) {
-            if (!f.getName().endsWith(".xml")) continue;
-            try {
-                String id = f.getName().substring(0, f.getName().indexOf(".xml"));
-                Document doc = processFile(id, f.getAbsolutePath());
-                em.persist(doc);
-            } catch (JAXBException e) {
-                logger.info(f.getName());
+        for (File f : files) {
+            Document doc;
+            if (f.getName().endsWith(".xml")) {
+                try {
+                    doc = processXmlFile(f);
+                } catch (JAXBException e) {
+                    logger.info(f.getName());
+                    continue;
+                }
+            } else if (f.getName().endsWith(".txt")) {
+                doc = processTxtFile(f);
+            } else {
+                continue;
             }
+            em.persist(doc);
+            if (FLUSH) flushToDisk(doc);
         }
         em.getTransaction().commit();
-
 
         String[] queries = new String[]{"книга", "президент предложил", "президенты предложили"};
         for (String q : queries) {
@@ -59,20 +70,43 @@ public class Runner {
         }
     }
 
-    private static void configureFTS() {
-        em.getTransaction().begin();
-        em.createNativeQuery("DROP TEXT SEARCH CONFIGURATION IF EXISTS public.fts_config;").executeUpdate();
-        em.createNativeQuery("DROP TEXT SEARCH DICTIONARY IF EXISTS public.russian_stem;").executeUpdate();
-        em.createNativeQuery("CREATE TEXT SEARCH CONFIGURATION public.fts_config ( COPY=pg_catalog.russian );").executeUpdate();
-        em.createNativeQuery("CREATE TEXT SEARCH DICTIONARY public.russian_stem ( TEMPLATE=snowball, language='russian', stopwords='russian' );").executeUpdate();
-        em.createNativeQuery("SET default_text_search_config = 'public.fts_config';").executeUpdate();
-        em.getTransaction().commit();
+    private static List<File> scanResources(File root) {
+        return scanResources(new ArrayList<File>(), root);
     }
 
-    private static Document processFile(String id, String filename) throws IOException, JAXBException {
-        InputStream xml = new URL("file:///" + filename).openStream();
-        JAXBElement<Text> document = unmarshaller.unmarshal(new StreamSource(xml), Text.class);
-        xml.close();
+    private static List<File> scanResources(List<File> files, File root) {
+        File[] children = root.listFiles();
+        if (children != null) {
+            for (File f : children) {
+                if (f.isDirectory()) {
+                    scanResources(files, f);
+                } else {
+                    files.add(f);
+                }
+            }
+        }
+        return files;
+    }
+
+    private static Document processTxtFile(File file) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        char[] buf = new char[1024];
+        int numRead = 0;
+        while ((numRead = reader.read(buf)) != -1) {
+            String readData = String.valueOf(buf, 0, numRead);
+            sb.append(readData);
+        }
+        reader.close();
+
+        String name = file.getName();
+        return new Document(new Integer(name.substring(0, name.indexOf(".txt"))), null, sb.toString());
+    }
+
+    private static Document processXmlFile(File file) throws IOException, JAXBException {
+        InputStream in = new URL("file:///" + file.getAbsolutePath()).openStream();
+        JAXBElement<Text> document = unmarshaller.unmarshal(new StreamSource(in), Text.class);
+        in.close();
 
         List<String> tags = document.getValue().getTags().getTag();
         String documentUrl = "";
@@ -91,23 +125,30 @@ public class Runner {
             sb.append('\n');
         }
 
-        Document doc = new Document();
-        doc.setUrl(documentUrl);
-        doc.setText(sb.toString());
-        doc.setId(new Integer(id));
+        String name = file.getName();
+        return new Document(new Integer(name.substring(0, name.indexOf(".xml"))), documentUrl, sb.toString());
+    }
 
-        // flush to disk
-        /*
-        File fullTextFile = new File("/tmp/opcorpora/docs/" + id + ".txt");
+    private static void flushToDisk(Document doc) throws IOException {
+        File fullTextFile = new File("/tmp/opcorpora/docs/" + doc.getId() + ".txt");
         if (!fullTextFile.getParentFile().exists()) {
             fullTextFile.getParentFile().mkdirs();
         }
         FileOutputStream fout = new FileOutputStream(fullTextFile);
-        fout.write(sb.toString().getBytes());
+        fout.write(doc.getText().getBytes());
         fout.close();
-        */
+    }
 
-        return doc;
+    private static void configureFTS() {
+        em.getTransaction().begin();
+        em.createNativeQuery("DROP TEXT SEARCH CONFIGURATION IF EXISTS public.fts_config;").executeUpdate();
+        em.createNativeQuery("DROP TEXT SEARCH DICTIONARY IF EXISTS public.russian_stem;").executeUpdate();
+        em.createNativeQuery("DROP INDEX IF EXISTS public.document_idx;").executeUpdate();
+        em.createNativeQuery("CREATE TEXT SEARCH CONFIGURATION public.fts_config ( COPY=pg_catalog.russian );").executeUpdate();
+        em.createNativeQuery("CREATE TEXT SEARCH DICTIONARY public.russian_stem ( TEMPLATE=snowball, language='russian', stopwords='russian' );").executeUpdate();
+        em.createNativeQuery("CREATE INDEX document_idx ON public.document USING gin(to_tsvector('russian', text));").executeUpdate();
+        em.createNativeQuery("SET default_text_search_config = 'public.fts_config';").executeUpdate();
+        em.getTransaction().commit();
     }
 
     @SuppressWarnings("unchecked")

c38a45a323dece34dae6479700a77619ad39b1d8
vtqveant <vtqveant@localhost>
2015-03-18 02:11:10 +0300

    a small test for csv writer

---
+++ src/main/java/ru/eventflow/nlp/ResultWriter.java
@@ -0,0 +1,12 @@
+package ru.eventflow.nlp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
+public interface ResultWriter {
+
+    void write(List<Integer> documentIds, Map<String, List<Integer>> matrix,
+               OutputStream out) throws IOException;
+}

8737a7a40621563d50c41f0329bfe5aa1768fcb7
Konstantin Sokolov <vtqveant@gmail.com>
2015-03-19 10:35:55 +0300

    basic gui started

---
+++ assessment/src/main/java/ru/eventflow/assessment/Runner.java
@@ -0,0 +1,18 @@
+package ru.eventflow.assessment;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        MainClientForm mainClientForm = new MainClientForm();
+
+        JFrame frame = new JFrame("Eventflow Assessment Tool");
+        frame.setPreferredSize(new Dimension(1024, 680));
+        frame.setContentPane(mainClientForm.getRootPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}

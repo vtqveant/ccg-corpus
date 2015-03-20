b841134e8aa438f379df5a1eaee8ce1d591c3e75
vtqveant <vtqveant@localhost>
2015-03-20 19:12:37 +0300

    contd.

--- assessment/src/main/java/ru/eventflow/fts/assessment/MainClientForm.java
+++ assessment/src/main/java/ru/eventflow/fts/assessment/MainClientForm.java
@@ -1,6 +1,7 @@
 package ru.eventflow.fts.assessment;
 
 import ru.eventflow.fts.datasource.Document;
+import ru.eventflow.fts.datasource.Query;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -22,85 +23,17 @@ public class MainClientForm extends JFrame {
     private JButton nonRelevantButton;
     private JPanel documentsPanel;
     private JPanel assessmentPanel;
-    private JTextPane relevanceTextPane;
+    private JTextPane queryDescriptionTextPane;
     private JTextPane documentTextPane;
     private JList docList;
+    private JButton prevQueryButton;
+    private JButton nextQueryButton;
+    private JLabel queryLabel;
 
     public MainClientForm() {
 
-        // where the GUI is created:
-        JMenuBar menuBar;
-        JMenu menu, submenu;
-        JMenuItem menuItem;
-        JRadioButtonMenuItem rbMenuItem;
-        JCheckBoxMenuItem cbMenuItem;
-
-        // create the menu bar.
-        menuBar = new JMenuBar();
-
-        // build the first menu.
-        menu = new JMenu("A Menu");
-        menu.setMnemonic(KeyEvent.VK_A);
-        menu.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
-        menuBar.add(menu);
-
-        // a group of JMenuItems
-        menuItem = new JMenuItem("A text-only menu item", KeyEvent.VK_T);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem("Both text and icon", new ImageIcon("images/middle.gif"));
-        menuItem.setMnemonic(KeyEvent.VK_B);
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
-        menuItem.setMnemonic(KeyEvent.VK_D);
-        menu.add(menuItem);
-
-        // a group of radio button menu items
-        menu.addSeparator();
-        ButtonGroup group = new ButtonGroup();
-        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_R);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        rbMenuItem = new JRadioButtonMenuItem("Another one");
-        rbMenuItem.setMnemonic(KeyEvent.VK_O);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        // a group of check box menu items
-        menu.addSeparator();
-        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
-        cbMenuItem.setMnemonic(KeyEvent.VK_C);
-        menu.add(cbMenuItem);
-
-        cbMenuItem = new JCheckBoxMenuItem("Another one");
-        cbMenuItem.setMnemonic(KeyEvent.VK_H);
-        menu.add(cbMenuItem);
-
-        // a submenu
-        menu.addSeparator();
-        submenu = new JMenu("A submenu");
-        submenu.setMnemonic(KeyEvent.VK_S);
-
-        menuItem = new JMenuItem("An item in the submenu");
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, ActionEvent.ALT_MASK));
-        submenu.add(menuItem);
-
-        menuItem = new JMenuItem("Another item");
-        submenu.add(menuItem);
-        menu.add(submenu);
-
-        // build second menu in the menu bar.
-        menu = new JMenu("Another Menu");
-        menu.setMnemonic(KeyEvent.VK_N);
-        menu.getAccessibleContext().setAccessibleDescription("This menu does nothing");
-        menuBar.add(menu);
-
+        // TODO rewrite if needed
+        JMenuBar menuBar = Stuff.getStuff();
         topPanel.add(menuBar);
 
 
@@ -123,9 +56,14 @@ public class MainClientForm extends JFrame {
             }
         });
 
-
-//        relevanceTextPane.setText("sdfsdfsdf");
-//        documentTextPane.setText("sdsdf");
+        List<Query> queries = em.createQuery("SELECT x FROM Query x ORDER BY x.id ASC", Query.class).getResultList();
+        if (queries.size() > 0) {
+            queryLabel.setText(queries.get(0).getOrthography());
+            queryDescriptionTextPane.setText(queries.get(0).getDescription());
+        }
+        if (queries.size() > 1) {
+            nextQueryButton.setEnabled(true);
+        }
     }
 
 
@@ -133,13 +71,10 @@ public class MainClientForm extends JFrame {
         return rootPanel;
     }
 
-
     class MyListCellRenderer extends JLabel implements ListCellRenderer {
         public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             Document document = (Document) value;
-            String line = (document.getText().length() < 20) ?
-                    document.getText() : document.getText().substring(0, 20) + "...";
-            setText(String.format("%6s %20s", document.getId(), line));
+            setText(String.format("%6s %20s", document.getId(), document.getUrl()));
             setOpaque(true);
             setBackground(isSelected ? Color.LIGHT_GRAY : Color.WHITE);
             return this;
--- datasource/src/test/java/ru/eventflow/fts/BootstrapDBTest.java
+++ datasource/src/test/java/ru/eventflow/fts/BootstrapDBTest.java
@@ -7,6 +7,7 @@ import ru.eventflow.fts.datasource.Query;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import java.util.Set;
 
 public class BootstrapDBTest {
 
@@ -31,6 +32,15 @@ public class BootstrapDBTest {
         assessment.setDocument(document);
         assessment.setRelevant(true);
         em.persist(assessment);
+
+        em.getTransaction().commit();
+
+        em.getTransaction().begin();
+
+        Query q = em.createQuery("SELECT x FROM Query x ORDER BY x.id ASC", Query.class).getSingleResult();
+        Set<Assessment> assessments = q.getAssessments();
+        System.out.println(q.getOrthography());
+        System.out.println(assessments.size());
 
         em.getTransaction().commit();
     }

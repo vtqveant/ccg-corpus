abaca62f8b459a38941811be787bb04339743aec
vtqveant <vtqveant@localhost>
2015-05-13 12:15:16 +0300

    small fixes

--- annotation/src/main/java/ru/eventflow/ccg/annotation/ui/view/DictionaryView.java
+++ annotation/src/main/java/ru/eventflow/ccg/annotation/ui/view/DictionaryView.java
@@ -7,13 +7,13 @@ public class DictionaryView extends JPanel {
 
     public DictionaryView() {
         setLayout(new BorderLayout());
-        setMinimumSize(new Dimension(200, 150));
+        setMinimumSize(new Dimension(-1, 150));
 
         JLabel label = new JLabel("dictionary");
 
         JScrollPane scrollPane = new JScrollPane(label);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setPreferredSize(new Dimension(200, 300));
+        scrollPane.setPreferredSize(new Dimension(-1, 300));
         add(scrollPane);
     }
 }

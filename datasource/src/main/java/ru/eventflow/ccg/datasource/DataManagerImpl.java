36ec2a5514c2c6d341c000ac5a6930bfef6b4afa
vtqveant <vtqveant@localhost>
2015-05-22 16:58:50 +0300

    contd.

--- datasource/src/main/java/ru/eventflow/ccg/datasource/DataManagerImpl.java
+++ datasource/src/main/java/ru/eventflow/ccg/datasource/DataManagerImpl.java
@@ -1,22 +1,41 @@
 package ru.eventflow.ccg.datasource;
 
 import ru.eventflow.ccg.datasource.model.corpus.Document;
+import ru.eventflow.ccg.datasource.model.dictionary.Form;
+import ru.eventflow.ccg.datasource.model.dictionary.Grammeme;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DataManagerImpl implements DataManager {
 
-    private final EntityManager enitityManager;
+    private final EntityManager entityManager;
 
     public DataManagerImpl(String persistenceUnitName) {
-        enitityManager = Persistence.createEntityManagerFactory(persistenceUnitName).createEntityManager();
+        entityManager = Persistence.createEntityManagerFactory(persistenceUnitName).createEntityManager();
     }
 
     @Override
     public List<Document> getAllDocuments() {
-        return enitityManager.createQuery("SELECT x FROM Document x ORDER BY x.id ASC", Document.class).getResultList();
+        return entityManager.createQuery("SELECT x FROM Document x ORDER BY x.id ASC", Document.class).getResultList();
+    }
+
+    public Map<Form, List<Grammeme>> getGrammemes(String form) {
+        TypedQuery<Form> query = entityManager.createQuery("SELECT x FROM Form x WHERE x.orthography = :orthography", Form.class);
+        query.setParameter("orthography", form);
+        List<Form> forms = query.getResultList();
+        Map<Form, List<Grammeme>> result = new HashMap<>();
+        for (Form f : forms) {
+            if (f.isLemma()) continue;
+            List<Grammeme> grammemes = f.getGrammemes();
+            grammemes.addAll(f.getLexeme().getLemma().getGrammemes());  // add common grammemes
+            result.put(f, grammemes);
+        }
+        return result;
     }
 
     @Override
--- datasource/src/test/java/ru/eventflow/ccg/datasource/BootstrapDBTest.java
+++ datasource/src/test/java/ru/eventflow/ccg/datasource/BootstrapDBTest.java
@@ -1,9 +1,14 @@
 package ru.eventflow.ccg.datasource;
 
 import org.junit.Test;
+import ru.eventflow.ccg.datasource.model.dictionary.Form;
+import ru.eventflow.ccg.datasource.model.dictionary.Grammeme;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Map;
 
 public class BootstrapDBTest {
 
@@ -12,6 +17,20 @@ public class BootstrapDBTest {
         EntityManager em = Persistence.createEntityManagerFactory(DataSource.DEFAULT).createEntityManager();
         em.getTransaction().begin();
         em.getTransaction().commit();
+    }
+
+    @Test
+    public void testDictionaryQueries() {
+        DataManagerImpl dataManager = new DataManagerImpl(DataSource.DEFAULT);
+        Map<Form, List<Grammeme>> results = dataManager.getGrammemes("были");
+
+        for (Map.Entry<Form, List<Grammeme>> entry : results.entrySet()) {
+            Form f = entry.getKey();
+            System.out.println(f.getOrthography() + " (" + f.getLexeme().getLemma().getOrthography() + ")");
+            for (Grammeme g : entry.getValue()) {
+                System.out.println('\t' + g.getName() + " - " + g.getDescription());
+            }
+        }
     }
 
 }

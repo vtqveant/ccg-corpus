499097fc72efe3294a26cd341e81b41edd5ea4cc
Konstantin Sokolov <vtqveant@gmail.com>
2015-06-04 00:22:56 +0300

    jtreetable tutorial link

--- annotation/src/main/java/ru/eventflow/ccg/annotation/ui/view/SearchView.java
+++ annotation/src/main/java/ru/eventflow/ccg/annotation/ui/view/SearchView.java
@@ -3,6 +3,8 @@ package ru.eventflow.ccg.annotation.ui.view;
 import javax.swing.*;
 import java.awt.*;
 
+// TODO JTreeTable howto: http://www.comp.nus.edu.sg/~cs3283/ftp/Java/swingConnect/tech_topics/tables-trees/tables-trees.html
+// part 2: http://www.comp.nus.edu.sg/~cs3283/ftp/Java/swingConnect/tech_topics/tables_trees_2/tables_trees_2.html
 public class SearchView extends JPanel {
 
     public SearchView() {

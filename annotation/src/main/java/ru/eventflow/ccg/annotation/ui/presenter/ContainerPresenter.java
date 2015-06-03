3058aa648e882a9e33691b7b4ae0d6fdff28adba
Konstantin Sokolov <vtqveant@gmail.com>
2015-06-03 18:25:48 +0300

    fix

--- annotation/src/main/java/ru/eventflow/ccg/annotation/ui/presenter/ContainerPresenter.java
+++ annotation/src/main/java/ru/eventflow/ccg/annotation/ui/presenter/ContainerPresenter.java
@@ -10,13 +10,10 @@ import ru.eventflow.ccg.datasource.model.corpus.Sentence;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ContainerPresenter implements Presenter<ContainerView> {
 
     private final ContainerView view;
-    private List<AnnotationPresenter> annotationPresenters = new ArrayList<>();
 
     /**
      * this setting is global and should apply to newly created tabs as well
@@ -46,7 +43,6 @@ public class ContainerPresenter implements Presenter<ContainerView> {
                 Sentence sentence = e.getSentence();
                 String title = "Sentence " + sentence.getId();
                 AnnotationPresenter presenter = new AnnotationPresenter(eventBus, sentence);
-                annotationPresenters.add(presenter);
                 presenter.getView().setGlossesVisible(glossesVisible);
                 view.addTab(title, presenter.getView());
             }
@@ -57,13 +53,14 @@ public class ContainerPresenter implements Presenter<ContainerView> {
             public void onEvent(SettingsEvent e) {
                 if (e.getSetting() == Setting.GLOSSES) {
                     glossesVisible = e.isEnabled();
-                    for (AnnotationPresenter presenter : annotationPresenters) {
-                        presenter.getView().setGlossesVisible(e.isEnabled());
+                    int tabCount = view.getTabbedPane().getTabCount();
+                    for (int i = 0; i < tabCount; i++) {
+                        AnnotationView annotationView = (AnnotationView) view.getTabbedPane().getComponentAt(i);
+                        annotationView.setGlossesVisible(e.isEnabled());
                     }
                 }
             }
         });
-
     }
 
     @Override
